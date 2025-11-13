151895454e5d9e4d2dd8caf5022b64f7cc5bee00
Queren <querenhalves@outlook.com>
2025-11-13 13:37:57 -0300

    :lipstick: ajustes na estilização

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,18 +2,18 @@
 
 function Home() {
     return (
-        <div className="bg-indigo-900 flex justify-center">
+        <div className="bg-purple-50 flex justify-center">
             <div className="container grid grid-cols-2 text-white">
                 <div className="flex flex-col gap-4 items-center justify-center py-4">
-                    <h2 className="text-5xl font-bold">Seja Bem Vindo(a)!</h2>
-                    <p className="text-xl">Expresse aqui seus pensamentos e opiniões</p>
+                    <h2 className="text-5xl font-bold text-purple-950">Seu Hub de Ideias</h2>
+                    <p className="text-xl text-purple-950">Onde cada post encontra seu lugar.</p>
                     <div className="flex justify-around gap-4">
-                        <div className="rounded text-white border-white border-solid border-2 py-2 px-4">Nova Postagem</div>
+                        <div className="rounded text-purple-950 border-purple-950 border-solid border-2 py-2 px-4">Nova Postagem</div>
                     </div>
                 </div>
 
                 <div className="flex justify-center">
-                    <img src="https://i.imgur.com/fyfri1v.png" alt="Imagem da Página Home" className=" w-2/3"/>
+                    <img src="https://i.imgur.com/56pPCUE.png" alt="Imagem da Página Home" className=" w-2/3 rounded-[8%_60%_45%_45%_/_40%_45%_8%_45%] shadow-lg"/>
                 </div>
             </div>
         </div>
