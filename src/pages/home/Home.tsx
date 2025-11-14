59ef07f7ce40f8d5f92b1376f69941906b89d73d
Queren <querenhalves@outlook.com>
2025-11-14 08:43:14 -0300

    :lipstick: modificações na estilização

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,11 +2,11 @@
 
 function Home() {
     return (
-        <div className="bg-purple-50 flex justify-center">
+        <div className="bg-purple-950 flex justify-center">
             <div className="container grid grid-cols-2 text-white">
                 <div className="flex flex-col gap-4 items-center justify-center py-4">
                     <h2 className="text-5xl font-bold text-purple-950">Seu Hub de Ideias</h2>
-                    <p className="text-xl text-purple-950">Onde cada post encontra seu lugar.</p>
+                    <p className="text-xl text-purple-950">Onde cada post encontra seu lugar</p>
                     <div className="flex justify-around gap-4">
                         <div className="rounded text-purple-950 border-purple-950 border-solid border-2 py-2 px-4">Nova Postagem</div>
                     </div>
