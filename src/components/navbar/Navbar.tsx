5fed982d5c331a3c9aa1190916800ccd8f875d3f
Queren <querenhalves@outlook.com>
2025-11-17 09:17:08 -0300

    :sparkles: estilização inicial do componente tema

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -19,8 +19,8 @@ function Navbar() {
                     <Link to="/home" className="text-2xl font-bold">PostHub</Link>
                     <div className="flex gap-4">
                         Postagens
-                        Temas
-                        Cadastrar Tema
+                        <Link to='/temas' className='hover:underline'>Temas</Link>
+                        <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link>
                         Perfil
                         <Link to="" onClick={logout} className="hover:underline">Sair</Link>
                     </div>
