340dd1f80d9f6190b8fc46e1f74774ccd1b7148a
Queren <querenhalves@outlook.com>
2025-11-27 11:31:33 -0300

    alterações no logo do site

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -10,10 +10,17 @@ function Footer() {
 
     if (usuario.token !== "") {
         component = (
-            <footer className="bg-linear-to-r from-[#2d3748] to-[#1f2937] text-gray-300 py-12">
+            <footer className="bg-linear-to-r from-[#2d3748] to-[#1f2937] text-white py-12">
                 <div className="max-w-6xl mx-auto px-6 flex  gap-10">
                     <div>
+                        <svg width="50" height="50" viewBox="0 0 64 64" fill="none"
+                            stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <circle cx="32" cy="32" r="28" />
+                            <path d="M23 23h11a6.5 6.5 0 0 1 0 13h-6l-5 6v-19z" />
+                        </svg>
                         <h2 className="text-xl font-semibold text-white mb-2">PostHub</h2>
+                        
                         <p className="text-sm leading-relaxed text-gray-400 max-w-[60%]">
                             Compartilhando ideias que inspiram e informam.
                             Junte-se à nossa comunidade de leitores e criadores.
@@ -22,9 +29,9 @@ function Footer() {
                     <div>
                         <h3 className="text-lg font-medium text-white mb-3">Links Rápidos</h3>
                         <ul className="space-y-2 text-sm">
-                            <li><Link to="/home" className="hover:text-white">Home</Link></li>
-                            <li><Link to="/postagens" className="hover:text-white">Postagens</Link></li>
-                            <li><Link to="/temas" className="hover:text-white">Temas</Link></li>
+                            <li><Link to="/home" className="hover:text-white text-gray-300">Home</Link></li>
+                            <li><Link to="/postagens" className="hover:text-white text-gray-300">Postagens</Link></li>
+                            <li><Link to="/temas" className="hover:text-white text-gray-300">Temas</Link></li>
                         </ul>
                     </div>
                 </div>
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,6 +2,7 @@ import { useContext, type ReactNode } from "react";
 import { Link, useNavigate } from "react-router-dom"
 import { AuthContext } from "../../contexts/AuthContext";
 import { ToastAlerta } from "../../utils/ToastAlerta";
+import { SignOutIcon, UserIcon } from "@phosphor-icons/react";
 
 function Navbar() {
     const navigate = useNavigate();
@@ -19,16 +20,25 @@ function Navbar() {
         component = (
             <div className="w-full bg-white border-gray-200 shadow-sm sticky top-0 z-50">
                 <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between text-gray-800">
-                    <Link to="/home" className="text-2xl font-semibold text-blue-950">
-                        PostHub
+                    <Link to="/home" className="text-2xl font-semibold flex justify-center gap-2 text-blue-950" title="PostHub Logo">
+                        <svg width="55" height="55" viewBox="0 0 64 64" fill="none"
+                            stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <circle cx="32" cy="32" r="28" />
+                            <path d="M23 23h11a6.5 6.5 0 0 1 0 13h-6l-5 6v-19z" />
+                        </svg>
+                       <span className="flex justify-center items-center">PostHub</span> 
                     </Link>
                     <nav className="hidden md:flex gap-8 text-[15px]">
                         <Link to="/home" className="hover:underline text-blue-950 transition">Home</Link>
                         <Link to="/postagens" className="hover:underline text-blue-950 transition">Postagens</Link>
                         <Link to="/temas" className="hover:underline text-blue-950 transition">Temas</Link>
-                        <Link to="/perfil" className="hover:underline text-blue-950 transition">Perfil</Link>
+                        
                     </nav>
-                    <Link to='' onClick={logout} className='border rounded px-4 py-2 bg-gray-800 text-white hover:bg-gray-700 cursor-pointer'>Sair</Link>
+                    <div className="flex gap-8">
+                    <Link to="/perfil" className="hover:underline text-blue-950 transition" title="Perfil"><UserIcon size={20} /></Link>
+                    <Link to='' onClick={logout} className='hover:underline text-blue-950 transition' title="Sair"><SignOutIcon size={20} /></Link>
+                    </div>
                 </div>
             </div>
         )
